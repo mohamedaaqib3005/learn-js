2173d250776f1c8e350d54b1ddb16c9773b8e78d
Aaqib <drop.mohamedaaqib@gmail.com>
2025-11-18 22:02:13 +0530

    first commit

---
+++ callback.js
@@ -0,0 +1,14 @@
+// callback functions
+
+setTimeout(function () {
+  console.log("timer");
+}, 5000);
+
+function x(y) {
+  console.log("x");
+  y();
+}
+
+x(function y() {
+  console.log("y");
+});
